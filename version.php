21c11f0144b073e3a015b696003c2a59da18a919
werner welte <werner.welte@haw-hamburg.de>
2017-11-06 16:36:39 +0100

    New Icon, again and again

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_else';
-$plugin->version = 2017110602;
+$plugin->version = 2017110607;
 $plugin->release = 'v0.3';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_ALPHA;
